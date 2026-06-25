dd0a2e287fcf2beb5300c651802117d42c0b3a60
Lama <deml72726@gmail.com>
2026-06-25 14:36:38 +0100

    polishin?wa9il

--- Assets/Lama/Scripts/Sheep/SheepJump.cs
+++ Assets/Lama/Scripts/Sheep/SheepJump.cs
@@ -40,6 +40,18 @@ public enum GameState { Playing, Won, Lost }
 public GameState currentState = GameState.Playing;
 public float loseSpinSpeedZ = 360f; // Degrees per second
 public float winSpinSpeedY = 360f;
+public GameObject shadow; // Assign your win effect prefab in the Inspector
+
+//SOUNDDECLARATION----
+[Range(0f, 1f)]
+public float chancesound;
+public AudioSource sheepJumpSound1;
+public AudioSource sheepJumpSound2;
+public AudioSource sheepJumpSound3;
+public AudioSource OIIAI;
+public AudioSource scream;
+bool firsttime = true;
+
 
     void Start()
     {
@@ -74,9 +86,11 @@ void Update()
         // If lost, spin wildly on the Z axis (like a tumble)
         rb.linearVelocity = new Vector2(walkSpeed, rb.linearVelocity.y);
         GetComponent<Collider2D>().enabled = false; // Optional: Disable the collider so it falls through the floor
+        scream.Play();
     }
     else if (currentState == GameState.Won)
     {
+        OIIAI.Play();
         // We still need to check the ground so it knows when it landed
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsFloor);
         anim.SetBool("IsGrounded", isGrounded);
@@ -86,7 +100,16 @@ void Update()
         {
             transform.Rotate(0, winSpinSpeedY * Time.deltaTime, 0);
         }
+        
     }
+    if (isGrounded)
+        {
+            shadow.SetActive(true);
+        }
+        else
+        {
+            shadow.SetActive(false);
+        }
 }
 
 void FixedUpdate()
@@ -131,7 +154,29 @@ void FixedUpdate()
     {
         // Apply the jump force upwards
         rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-    }
+        chancesound = Random.Range(0f, 1f); // Random number between 0 and 1
+// Play a random jump sound
+    if (firsttime)
+    {
+        if (chancesound > 0.5f) // 30% chance to play a sound
+        {
+            int randomSound = Random.Range(1, 4); // Random number between 1 and 3
+            switch (randomSound)
+            {
+                case 1:
+                    sheepJumpSound1.Play();
+                    break;
+                case 2:
+                    sheepJumpSound2.Play();
+                    break;
+                case 3:
+                    sheepJumpSound3.Play();
+                    break;
+            }
+        }
+        firsttime = false; // Set firsttime to false after the first jump
+    }
+    }
 
     // This draws helpful lines in the Unity Editor so you can see the edge detection
     private void OnDrawGizmosSelected()
